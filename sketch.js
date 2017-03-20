eda596984ee86d1b9b1d1aed70df01c4063f0b26
Cyron Completo <cyron.completo@gmail.com>
2017-03-20 15:24:34 -0600

    rotating the entire screen off-center

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup() {
 	frameRate(30);
 	for (var i = 0; i < difficulty; i++) particles.push(new Particle(ColorID(randomWholeNum(3))));
 	playerObj = new Player(polyRad, polyPoints);
+	gameState = new Game();
 }
 
 function randomWholeNum(range) {
@@ -30,91 +31,45 @@ function recursePoly(x, y, theta)
 	var a = (width/2) + cos(theta+angle)*polyRad;
 	var b = (height/2) + sin(theta+angle)*polyRad;
 	recursePoly(a, b, theta+angle);
-	vertDist = dist(x, y, a, b);
-	stroke(100);
-	var m = (b-y)/(a-x);
-	var xPrime = -m*(b-y-(x/m));
-	line(x, y, xPrime, b);
+	// vertDist = dist(x, y, a, b);
+	// stroke(100);
+	// var m = (b-y)/(a-x);
+	// var xPrime = -m*(b-y-(x/m));
+	// line(x, y, xPrime, b);
 	stroke(255);
 	return line(x, y, a, b);
 }
 
 function draw() 
 {
+	// translate(width/2, height/2);
 	var hit;
 	var hitCounter = 0;
 	background('black');
-	var x = recursePoly((width/2) + cos(0)*polyRad, (height/2) + sin(0)*polyRad, 0);
-	playerObj.display();
 
 	if (keyIsDown(LEFT_ARROW)) {
-		// if (dist(playerObj.x, playerObj.y, ) {}
-		playerObj.x-=10;
+		gameState.deg -= 10;
 	}
 
 	if (keyIsDown(RIGHT_ARROW)) {
-		playerObj.x+=10;
-		// playerObj.y -= (sin(3.14/16)/cos(3.14/16)); 
+		gameState.deg += 10;
 	}
-	/*
-	if (playerObj.lives > 0) 
-	{
-		if (colorCounter >= 10){
-			playerObj.color = "blue";
-		}
-		if (counter1 >= 20) {
-			counter1 = 0;
-			counter2++;
-		}
-		if (counter2 >= difficulty) counter2 = difficulty;
 
-		background('black');
+	rotate(radians(gameState.deg));
 
-		if (keyIsDown(LEFT_ARROW) && (playerObj.x - 10 >= 0)) {
-			playerObj.x -= 10;
-		}
+	var x = recursePoly((width/2) + cos(0)*polyRad, (height/2) + sin(0)*polyRad, 0);
 
-		if (keyIsDown(RIGHT_ARROW) && (playerObj.x + 10 < 600)) {
-			playerObj.x += 10;
-		}
 
-		playerObj.display();
+	// if (keyIsDown(LEFT_ARROW)) {
+	// 	playerObj.update(-10);
+	// }
 
-		for (var i = 0; i < counter2; i++) 
-		{
-			particles[i].y+=10;
-			particles[i].display();
+	// if (keyIsDown(RIGHT_ARROW)) {
+	// 	playerObj.update(10);
+	// }
 
-			hit = collideRectCircle(playerObj.x, playerObj.y, playerObj.sizeX, playerObj.sizeY, particles[i].x, particles[i].y, particles[i].size);
-			if (hit) {
-				colorCounter = 0;
-				playerObj.color = particles[i].color;
-				playerObj.interaction(playerObj.color);
-				particles[i].size = 0;
-				particles[i] = new Particle(ColorID(randomWholeNum(3)));
-			}
-			if (particles[i].y >= windowHeight) {
-				particles[i] = new Particle(ColorID(randomWholeNum(3)));
-			}
-		}
-		counter1++;
-		colorCounter++;
-		document.getElementById('points').innerHTML = "Points: " + playerObj.points;
-		document.getElementById('lives').innerHTML = "Lives: " + playerObj.lives;
-	} else {
-		if (clearer) {
-			clear();
-			clearer = false;
-		}
-		textSize(32);
-		text("GAME OVER! Press any key to try again", 10, 150);
-		fill(255);
-		if (keyIsPressed === true) {
-			playerObj = new Player();
-			clearer = true;
-		}
-	}
-	*/
+	playerObj.display();
+
 }
 
 
@@ -141,13 +96,22 @@ function Player(r, n)
 	this.color = "blue";
 	this.lives = 3;
 	this.points = 0;
+	this.deg = 0;
 
 	this.display = function()
 	{
-		translate(((width/2)+(cos(angle*6)))+(50), (((height/2)+(sin(angle*6)))+(polyRad-50)));
+		// translate(((width/2)+(cos(angle*6)))+(50), (((height/2)+(sin(angle*6)))+(polyRad-50)));
+		translate((width/2), (height/2)+150);
 		fill(this.color);
-		rotate(-(3.14/8));
+		// rotate(-(3.14/8));
+
+		rotate(this.deg);
 		rect(this.x, this.y, this.sizeX, this.sizeY);
+	}
+
+	this.update = function(deg)
+	{
+		this.deg += radians(deg);
 	}
 
 	this.interaction = function(color)
@@ -171,4 +135,8 @@ function Particle(color)
 		fill(this.color);
 		ellipse(this.x, this.y, this.size, this.size);
 	}
+}
+
+function Game() {
+	this.deg = 0;
 }
